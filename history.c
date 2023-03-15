02354f196dda597c53f8ef5ce2ce4d542d3b89d2
imekaferdy <imekaferdy20@gmail.com>
2023-03-15 19:34:34 +0100

    updated

--- history.c
+++ history.c
@@ -1,86 +1,143 @@
 #include "shell.h"
 
-/* Global variables */
-static int no_init_history;
-static list_t *history_head;
-static list_t *last_cmd;
-
 /**
- * get_history_addrss - Return the address oof history head
+ * get_history_file - gets the history file
+ * @info: parameter struct
  *
- * Return: Address of history head
-*/
+ * Return: allocated string containg history file
+ */
 
-list_t **get_history_addrss()
+char *get_history_file(info_t *info)
 {
-	if (no_init_history == 1)
-	{
-		history_head = NULL;
-		last_cmd = NULL;
-		no_init_history = 0;
-	}
+	char *buf, *dir;
 
-	return (&history_head);
+	dir = _getenv(info, "HOME=");
+	if (!dir)
+		return (NULL);
+	buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
+	if (!buf)
+		return (NULL);
+	buf[0] = 0;
+	_strcpy(buf, dir);
+	_strcat(buf, "/");
+	_strcat(buf, HIST_FILE);
+	return (buf);
 }
 
 /**
- * get_last_cmd_addrss - Return the address oof history head
+ * write_history - creates a file, or appends to an existing file
+ * @info: the parameter struct
  *
- * Return: Address of last entered command
-*/
-list_t **get_last_cmd_addrss()
+ * Return: 1 on success, else -1
+ */
+int write_history(info_t *info)
 {
-	if (no_init_history == 1)
+	ssize_t fd;
+	char *filename = get_history_file(info);
+	list_t *node = NULL;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
+	free(filename);
+	if (fd == -1)
+		return (-1);
+	for (node = info->history; node; node = node->next)
 	{
-		history_head = NULL;
-		last_cmd = NULL;
-		no_init_history = 0;
+		_putsfd(node->str, fd);
+		_putfd('\n', fd);
 	}
-
-	return (&last_cmd);
-}
-
-/**
- * _history - Prints the all the commands entered by the user
- *
- * Return: 1 on success
-*/
-int _history(void)
-{
-	list_t *curr;
-	char *str_num;
-	int i, len;
-	int count = *get_history_lines_count() % 4096;
-
-	for (curr = *get_history_addrss(); curr != NULL; curr = curr->next)
-	{
-		str_num = num_to_str(count++);
-		len = _puts(str_num);
-		for (i = len; i < 7; i++) /* 5 per number, 2 aditional */
-			_puts(" ");
-		_puts(curr->str);
-		_puts("\n");
-		free(str_num);
-	}
-
+	_putfd(BUF_FLUSH, fd);
+	close(fd);
 	return (1);
 }
 
 /**
- * handle_history - Adds a command to the history
- * @buff: User's input
-*/
-void handle_history(char *buff)
+ * read_history - reads history from file
+ * @info: the parameter struct
+ *
+ * Return: histcount on success, 0 otherwise
+ */
+int read_history(info_t *info)
 {
-	/* Only adds a command if is different from the previous one */
-	if (last_cmd == NULL || buff[0] != ' ' || _strcmp(last_cmd->str, buff) != 0)
-		last_cmd = add_node_end(get_history_addrss(), buff);
+	int i, last = 0, linecount = 0;
+	ssize_t fd, rdlen, fsize = 0;
+	struct stat st;
+	char *buf = NULL, *filename = get_history_file(info);
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	free(filename);
+	if (fd == -1)
+		return (0);
+	if (!fstat(fd, &st))
+		fsize = st.st_size;
+	if (fsize < 2)
+		return (0);
+	buf = malloc(sizeof(char) * (fsize + 1));
+	if (!buf)
+		return (0);
+	rdlen = read(fd, buf, fsize);
+	buf[fsize] = 0;
+	if (rdlen <= 0)
+		return (free(buf), 0);
+	close(fd);
+	for (i = 0; i < fsize; i++)
+		if (buf[i] == '\n')
+		{
+			buf[i] = 0;
+			build_history_list(info, buf + last, linecount++);
+			last = i + 1;
+		}
+	if (last != i)
+		build_history_list(info, buf + last, linecount++);
+	free(buf);
+	info->histcount = linecount;
+	while (info->histcount-- >= HIST_MAX)
+		delete_node_at_index(&(info->history), 0);
+	renumber_history(info);
+	return (info->histcount);
 }
 
 /**
- * free_history - Frees the memory used by history list
-*/
-void free_history(void)
+ * build_history_list - adds entry to a history linked list
+ * @info: Structure containing potential arguments. Used to maintain
+ * @buf: buffer
+ * @linecount: the history linecount, histcount
+ *
+ * Return: Always 0
+ */
+int build_history_list(info_t *info, char *buf, int linecount)
 {
-	free_list(*get_history_addrss());
+	list_t *node = NULL;
+
+	if (info->history)
+		node = info->history;
+	add_node_end(&node, buf, linecount);
+
+	if (!info->history)
+		info->history = node;
+	return (0);
+}
+
+/**
+ * renumber_history - renumbers the history linked list after changes
+ * @info: Structure containing potential arguments. Used to maintain
+ *
+ * Return: the new histcount
+ */
+int renumber_history(info_t *info)
+{
+	list_t *node = info->history;
+	int i = 0;
+
+	while (node)
+	{
+		node->num = i++;
+		node = node->next;
+	}
+	return (info->histcount = i);
 }
